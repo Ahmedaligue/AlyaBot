a46b1b6abc0c6132043f426c3caa77ef0a06d9e8
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2026-01-01 01:06:21 +0100

    Update sub-bots_bots.js

--- plugins/sub-bots_bots.js
+++ plugins/sub-bots_bots.js
@@ -1,46 +1,59 @@
 import ws from 'ws';
 
 async function handler(m, { conn: _envio, usedPrefix }) {
-const msgTxt = await ("✅ Prestar el bot para unirlo a grupos")
-const msgTxt2 = await ("Tiempo activo")
-const msgTxt3 = await ("*Ups no se detectaron sub-bots, verifique mas tarde.*")
-const msgTxt4 = await ("Username")
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+  const msgTxt = await ("✅ إعارة البوت للانضمام إلى المجموعات");
+  const msgTxt2 = await ("مدة التشغيل");
+  const msgTxt3 = await ("*عذراً، لم يتم العثور على أي بوتات فرعية، تحقق لاحقاً.*");
+  const msgTxt4 = await ("اسم المستخدم");
 
-function convertirMsADiasHorasMinutosSegundos(ms) {
-var segundos = Math.floor(ms / 1000);
-var minutos = Math.floor(segundos / 60);
-var horas = Math.floor(minutos / 60);
-var días = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-var resultado = "";
-if (días !== 0) {
-resultado += días + " días, ";
-}
-if (horas !== 0) {
-resultado += horas + " horas, ";
-}
-if (minutos !== 0) {
-resultado += minutos + " minutos, ";
-}
-if (segundos !== 0) {
-resultado += segundos + " segundos";
-}
-return resultado;
+  const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+
+  function convertirMsADiasHorasMinutosSegundos(ms) {
+    var segundos = Math.floor(ms / 1000);
+    var minutos = Math.floor(segundos / 60);
+    var horas = Math.floor(minutos / 60);
+    var días = Math.floor(horas / 24);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    var resultado = "";
+    if (días !== 0) {
+      resultado += días + " يوم، ";
+    }
+    if (horas !== 0) {
+      resultado += horas + " ساعة، ";
+    }
+    if (minutos !== 0) {
+      resultado += minutos + " دقيقة، ";
+    }
+    if (segundos !== 0) {
+      resultado += segundos + " ثانية";
+    }
+    return resultado;
+  }
+
+  const message = users.map((v, index) => {
+    const botConfig = global.db.data.users[v.user.jid] || {};
+    const botNumber = botConfig.privacy ? `${msgTxt4}: ` : `wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado`;
+    const prestarStatus = botConfig.privacy ? '' : (botConfig.prestar ? msgTxt : '');
+    return `📡 ${botNumber} (${v.user.name || '-'})\n*⏳ ${msgTxt2} :* ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "غير معروف"}\n${prestarStatus}`;
+  }).join('\n\n');
+
+  const replyMessage = message.length === 0 ? msgTxt3 : message;
+  const totalUsers = users.length;
+
+  const responseMessage = await (`*🤖 إليك قائمة ببعض البوتات الفرعية المتصلة 🤖️*\n\n*👉🏻 يمكنك التواصل معهم لمعرفة إن كانوا سينضمون إلى مجموعتك*\n\n*نرجو منك:*\n*1.- أن تكون لطيفاً ✅*\n*2.- لا تُلح ولا تجادل ✅*\n\n*✳️ إذا ظهر النص التالي فارغاً فهذا يعني أنه لا يوجد أي بوت فرعي متاح حالياً، حاول لاحقاً.*\n\n*_⚠ ملاحظة: هذه الجلسات تخص أشخاصاً لا علاقة لهم بفريق AlyaBot، لذلك لسنا مسؤولين عما يحدث هناك._*\n\n*🤖 عدد البوتات الفرعية المتصلة :* `) + `${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+
+  await _envio.sendMessage(m.chat, {
+    text: responseMessage,
+    contextInfo: {
+      mentionedJid: _envio.parseMention(responseMessage)
+      /*, externalAdReply: { mediaUrl: null, mediaType: 1, description: null, title: wm, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: null, sourceUrl: null}*/
+    }
+  }, { quoted: m });
 }
 
-const message = users.map((v, index) => {
-const botConfig = global.db.data.users[v.user.jid] || {};
-const botNumber = botConfig.privacy ? `${msgTxt4}: ` : `wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado`
-const prestarStatus = botConfig.privacy ? '' : (botConfig.prestar ? msgTxt : '');
-return `📡 ${botNumber} (${v.user.name || '-'})\n*⏳ ${msgTxt2} :* ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}\n${prestarStatus}`}).join('\n\n');
-const replyMessage = message.length === 0 ? msgTxt3 : message;
-const totalUsers = users.length;
-const responseMessage = await (`*🤖 Aquí tienes la lista de algunos sub bots  🤖️*\n\n*👉🏻 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Te pedimos de favor que:*\n*1.- Seas amable ✅*\n*2.- No insistas ni discutas ✅*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n*_⚠ NOTA: Estas sesiones son de personas cuya identidad es ajena al Staff de AlyaBot Por lo que no nos hacemos responsables de lo que pase ahi .._*\n\n*🤖 SUBBOTS CONECTADO :* `) + `${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
-await _envio.sendMessage(m.chat, {text: responseMessage, contextInfo: { mentionedJid: _envio.parseMention(responseMessage)/*, externalAdReply: { mediaUrl: null, mediaType: 1, description: null, title: wm, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: null, sourceUrl: null}*/}}, { quoted: m });
-}
-handler.command = handler.help = ['listjadibot', 'bots'];
+handler.command = handler.help = ['listjadibot', 'bots', 'قائمة_البوتات'];
 handler.tags = ['jadibot'];
+
 export default handler;
